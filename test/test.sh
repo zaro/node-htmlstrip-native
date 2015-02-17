4c5f9aeb3fb4ab9339ff374ff4155bd39137a41b
Svetlozar Argirov <zarrro@gmail.com>
2015-02-17 10:55:13 +0200

    Add a longer test

--- test/test.sh
+++ test/test.sh
@@ -6,3 +6,4 @@ TDIR=`cd $(dirname "$0"); pwd`
 node $TDIR/test_acc.js
 node $TDIR/test_entities.js
 node $TDIR/test_strip.js
+node $TDIR/test_strip_random.js
---
+++ test/test_strip_random.js
@@ -0,0 +1,54 @@
+var hs = require('../')
+
+function getRandomChar() {
+  var s=  String.fromCharCode( Math.floor(Math.random() * (127 - 32)) + 32 );
+  return s;
+}
+
+function generateTestString(length) {
+  var str="", exp="", inTag=false, inTagNext=false;
+  while(length) {
+    var c = getRandomChar();
+    var e = c;
+    if(c == '&'){
+      c += 'amp;';
+      e = '&';
+    }
+    if( c == '<') {
+      if(inTag) continue;
+      inTagNext= true;
+      e = ' ';
+    }
+    if( c == '>') {
+      inTagNext = false;
+      e = undefined;
+    }
+    str += c;
+    if(!inTag && e) {
+      exp += e;
+    }
+    inTag = inTagNext;
+    length--;
+  }
+  return { str: str, exp: exp };
+}
+
+
+console.log("TEST random tag stripping")
+for(var i=0; i < 10000; ++i){
+  var t = generateTestString(100);
+  var converted = hs.html_strip(t.str, {});
+  var result = (t.exp == converted);
+  if(!result){
+    console.log(
+      "ORIG> ❮" + t.str + "❯\n" +
+      "EXP > ❮" + t.exp + "❯\n" +
+      "CONV> ❮" + converted + "❯\n" +
+      "RES > "  + result
+    );
+
+    console.log('FAIL!');
+    process.exit(-1);
+  }
+}
+console.log('OK');
